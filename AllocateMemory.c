d0a7b9a50c8b79af32a6157d89614ee6547bf710
sakdjsalkj <1527215026@qq.com>
2018-11-29 22:46:13 +0800

    1

--- AllocateMemory.c
+++ AllocateMemory.c
@@ -16,14 +16,14 @@ Node* GetTarget(Node* Head,Node *p,int size){
         p->next = NULL;
         return p;
     }else{
-        p->size = size;
+        p->size -= size;
         Node *new = malloc(sizeof(Node));
         new->size = size;
         new->loc = p->loc;
+        p->loc += size;
         new->next = NULL;
-        return NULL;
+        return new;
     }
-
 }
 Node* firstNode(Node* Free,int size){
     Node *p = Free->next;
@@ -35,37 +35,45 @@ Node* firstNode(Node* Free,int size){
         if(p->size < size){
             p = p->next;
         }
+        else break;
+    }
+    	if(!p)
+    		return NULL;
         judge = p->next;
         while (judge)
         {
             if ((judge->size < p->size) && judge->size >= size)
                 p = judge;
+            else
+            	break;
         }
-    }
     return GetTarget(Free, p, size);
 }
+void Adjust(Node* pre,Node* next){
+    if(pre->loc == next->loc)
+        next->loc += pre->size;
+}
 void Allocate(Node* Head, Node* p){
-    Node *pre = NULL;
+    Node *pre = Head;
     Node *target = Head->next;
     if(!target){
         Head->next = p;
         p->next = NULL;
         return;
     }
-    while(1){
-        if(target->loc < p->loc){
-            pre = target;
-            target = target->next;
-        }
-        p->next = target;
-        pre->next = p;
-        return;
-    }
+    while(target && (target->loc < p->loc)){
+    	pre = target;
+    	target = target->next;
+	}
+		p->next = target;	// even though !target is ok
+		pre->next = p;
 }
 void Request(int size){
     Node *p = firstNode(Free, size);
-    if(!p)
-        return;
+    if(!p){
+    	printf("Request Memory Fail!\n");
+    	return;	
+	}
     Allocate(Allocation, p);
 }
 void Merge(Node* pre,Node* target,Node* p){
@@ -82,7 +90,7 @@ void Merge(Node* pre,Node* target,Node* p){
                 target = pre->next;
             }
         }
-    }else if(p->loc+p->size == target->loc){
+    }else if(target && p->loc+p->size == target->loc){
         target->loc = p->loc;
         target->size += p->size;
         free(p);
@@ -114,6 +122,9 @@ void Insert(Node* Head,Node* p){
         if(target->loc <= p->loc){
             pre = target;
             target = target->next;
+        }else
+        {
+            break;
         }
     }
     Merge(pre, target, p);
@@ -127,7 +138,8 @@ Node* Findloc(Node* Head,int loc){
         if(p->loc != loc){
             pre = p;
             p = p->next;
-        }
+        }else
+        	break;
     }
     if(!p){
         return NULL;
@@ -136,9 +148,77 @@ Node* Findloc(Node* Head,int loc){
     p->next = NULL;
     return p;
 }
-void Free(Node* Head,int loc){
+void FreeMemory(Node* Head,int loc){
     Node *p = Findloc(Head, loc);
-    if(!p)
-        return;
+    if(!p){
+    	printf("Free Memory Fail!\n");
+    	return;	
+	}
     Insert(Free, p);
+}
+void Display(Node* Head){
+    Node *p = Head->next;
+    while(p){
+        printf("loc:%d,size:%d ", p->loc, p->size);
+        p = p->next;
+    }
+    printf("\n");
+}
+void InitMemory(Node* Free,int loc,int size){
+    Free->loc = loc;
+    Free->size = size;
+    Node *new = (Node *)malloc(sizeof(Node));
+    new->loc = loc;
+    new->size = size;
+    new->next = NULL;
+    Free->next = new;
+}
+void Menu(){
+    printf("1.Initialize Memory\n");
+    printf("2.Request Memory\n");
+    printf("3.Free Memory\n");
+    printf("0.exit\n");
+    printf("Please select:");
+}
+int main(){
+    Free = (Node *)malloc(sizeof(Node));
+    Allocation = (Node *)malloc(sizeof(Node));
+    Free->next = NULL;
+    Allocation->next = NULL;
+    int x = 0,loc = 0,size = 0;
+    while(1){
+        Menu();
+        scanf("%d", &x);
+        switch (x){
+        case 1 :
+            printf("Please input the loc and size of Memory:");
+            scanf("%d%d", &loc, &size);
+            InitMemory(Free, loc, size);
+            printf("Below is the current FreeList:\n");
+            Display(Free);
+            break;
+        case 2:
+            printf("Please input the size you request:");
+            scanf("%d", &size);
+            Request(size);
+            printf("Below is the current FreeList:\n");
+            Display(Free);
+            printf("Below is the current AllocationList:\n");
+            Display(Allocation);
+            break;
+        case 3:
+            printf("Please input the loc you want to free:");
+            scanf("%d", &loc);
+            FreeMemory(Allocation, loc);
+            printf("Below is the current FreeList:\n");
+            Display(Free);
+            printf("Below is the current AllocationList:\n");
+            Display(Allocation);
+            break;
+        case 0:
+            printf("Thanks to use!\n");
+            exit(1);
+            break;
+        }
+    }
 }
